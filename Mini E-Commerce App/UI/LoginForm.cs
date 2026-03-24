85890946fb93cdac755b97261f45108dcd81bddf
morovani <jeovanniakinbaloye@gmail.com>
2026-03-24 14:12:25 -0300

    Add project files.

---
+++ Mini E-Commerce App/UI/LoginForm.cs
@@ -0,0 +1,10 @@
+namespace Mini_E_Commerce_App
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Mini E-Commerce App/UI/MainShoppingForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mini_E_Commerce_App
+{
+    public partial class MainShoppingForm : Form
+    {
+        public MainShoppingForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
